fb86660fd17151add879cb429310fb5441a68bf1
Alexander <44374132+ADNikulin@users.noreply.github.com>
2024-07-26 02:03:21 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ terraform {
   required_version = ">=0.13"
 }
 
+# provider "docker" {}
+
 provider "docker" {
   host     = "ssh://root@89.169.161.55:22"
   ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "-i", "/home/user/.ssh/id_ed25519"]
@@ -25,4 +27,27 @@ resource "random_password" "random_string" {
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = true
+}
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx1" {
+  image = docker_image.nginx.image_id
+  name  = "hello_world_test2"
+
+  ports {
+    internal = 80
+    external = 9090
+  }
 }
